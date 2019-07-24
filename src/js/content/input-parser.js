2feeb1bf903aa5fdd3f1375d2bb177a7a20e2637
Altair Wei <altair_wei@outlook.com>
2019-07-24 16:43:17 +0800

    NEW: add input-parser-wiznoteplus.js

--- src/js/content/input-parser.js
+++ src/js/content/input-parser.js
@@ -9,19 +9,7 @@
       case 'NativeApp':
         return InputParser_Default.parse(params);
       case 'WizNotePlus':
-      {
-        //FIXME: Move the parser to WizNotePlus clipping-handler
-        let result = InputParser_Default.parse(params);
-        // "index" is used to identify the entry point of document
-        result.info.filename = ['index', result.info.format].join('.');
-        // Keep all paths relative to $WIZNOTE_TEMP/webclipping
-        result.path.saveFolder = result.info.clipId;                     /** the path to place index.html and assetFolder */
-        result.path.assetFolder = result.info.clipId + "/index_files";   /** the path to place asset files */
-        result.path.assetRelativePath = "index_files";                   /** the path is relative to index.html */
-        //
-        result.needSaveTitleFile = false;
-        return result;
-      }
+        return InputParser_WizNotePlus.parse(params);
     }
     throw new Error('Should not reach here');
   }
