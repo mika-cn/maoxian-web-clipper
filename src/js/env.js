1e4c9ca740b5cfc69d222094fc228a5d39436df6
mika <mika@nothing.org>
2020-01-21 09:55:06 +0800

    bump version 0.1.47

--- src/js/env.js
+++ src/js/env.js
@@ -15,7 +15,7 @@
 
   return {
     logLevel: "debug",
-    version: '0.1.46',
+    version: '0.1.47',
     minNativeAppVersion: '0.2.2',
     websiteRoot: websiteRoot,
     projectRoot: projectRoot,
