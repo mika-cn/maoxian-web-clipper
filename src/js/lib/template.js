44c96dba496299a3c1f60433135bbc0d0e902fef
mika <mika@nothing.org>
2020-02-22 15:41:02 +0800

    fix html template

--- src/js/lib/template.js
+++ src/js/lib/template.js
@@ -103,7 +103,7 @@
   <html id="${v.htmlId}" class="${v.htmlClass}">
     <!-- OriginalSrc: ${v.info.link} -->
     <head>
-      <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <title>${v.info.title}</title>
