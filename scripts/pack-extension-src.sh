282c377caacc67d45ab85ef5c4c41b27c6c8d231
mika <mika@nothing.org>
2020-05-21 14:54:11 +0800

    IMPROVE scripts
      * auto bump version
      * move scripts to subfolder

--- scripts/pack-extension-src.sh
+++ scripts/pack-extension-src.sh
@@ -3,13 +3,28 @@
 
 # AMO requires us to provide source code.
 
-# usage ./pack-extension-src.sh 0.0.1
+# usage ./pack-extension-src.sh
 
+file_dir=$(dirname $(realpath "$0"))
+root_dir=$(dirname $file_dir)
 
-version=$1
+if [ "$PWD" = $root_dir ]; then
+  echo "Working Dir OK!"
+else
+  echo "[EXIT] This script can only be executed in $root_dir"
+  exit 1
+fi
+
+#================================================
+# pack extension source
+#================================================
+
+version=$(grep -e \"version\": package.json | cut -d \" -f 4)
+
+echo "Version: $version"
 
 # project root
-dir=$(dirname $(realpath "$0"))
+dir=$root_dir
 src=$(realpath "$dir/src")
 dist=$(realpath "$dir/dist/extension-src")
 
@@ -32,4 +47,3 @@ cd $dist/$extname
 zip -r $dist/$archive *
 rm -rf $dist/$extname
 echo "Done! ($dist/$archive)"
-
---
+++ scripts/pack-extension.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# This script will pack extension to zip file
+# Required environment variables are:
+#   MX_CHROME_ID (public key of MaoXian)
+#   MX_FIREFOX_ID (extension id of MaoXian)
+#
+# Usage: ./scripts/bump-version.sh new_version
+
+file_dir=$(dirname $(realpath "$0"))
+root_dir=$(dirname $file_dir)
+
+if [ "$PWD" = $root_dir ]; then
+  echo "Working Dir OK!"
+else
+  echo "[EXIT] This script can only be executed in $root_dir"
+  exit 1
+fi
+
+#================================================
+# pack extension
+#================================================
+
+if [ "$MX_CHROME_ID" = "" ]; then
+  echo "Environment variable MX_CHROME_ID is empty"
+  exit 1
+fi
+
+if [ "$MX_FIREFOX_ID" = "" ]; then
+  echo "Environment variable MX_FIREFOX_ID is empty"
+  exit 1
+fi
+
+npm run build-all
+
+exit 0
