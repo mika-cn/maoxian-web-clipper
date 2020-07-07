10ae16f96bc3baf2333d7fdc79a92c4408666787
mika <mika@nothing.org>
2020-07-07 14:23:45 +0800

    FIX typo

--- src/js/capturer/iframe.js
+++ src/js/capturer/iframe.js
@@ -80,7 +80,7 @@ async function capture(node, opts) {
 
   const msgType = saveFormat === 'html' ? 'frame.toHtml' : 'frame.toMd';
   try {
-    const {fromCache, result} = await ExtMsg.sendToBackend('clippibng', {
+    const {fromCache, result} = await ExtMsg.sendToBackend('clipping', {
       type: msgType,
       frameId: frame.frameId,
       frameUrl: frame.url,
