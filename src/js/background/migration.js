bea151d8ab713dd703f2a6ed3c5459dc4c525270
mika <mika@nothing.org>
2020-10-20 12:06:21 +0800

    FIX migration
      * serialize migrations
      * assign new attribute rather than rely on fixKeys()

--- src/js/background/migration.js
+++ src/js/background/migration.js
@@ -5,60 +5,61 @@ import T           from '../lib/tool.js';
 import MxWcStorage from '../lib/storage.js';
 import MxWcConfig  from '../lib/config.js';
 
-function perform() {
-  migrateV0134();
-  migrateV0146();
-  migrateV0147();
+async function perform() {
+  await migrateOldVersion();
 }
 
-function migrateV0134(){
+async function migrateOldVersion() {
+  await migrateV0134();
+  await migrateV0146();
+  await migrateV0147();
+}
+
+async function migrateV0134(){
   const {version} = ENV;
   if(T.isVersionGteq(version, '0.1.36')) {
     return ;
   }
   const key = 'mx-wc-config-migrated-0.1.34'
-  MxWcStorage.get(key, false).then((isMigrated) => {
-    if(isMigrated) {
-      console.info(key);
-    } else {
-      migrateConfigToV0134();
-      migrateStorageToV0134();
-      MxWcStorage.set(key, true);
-    }
-  });
+  const isMigrated = await MxWcStorage.get(key, false);
+  if(isMigrated) {
+    console.info(key);
+  } else {
+    await migrateConfigToV0134();
+    await migrateStorageToV0134();
+    await MxWcStorage.set(key, true);
+  }
 }
 
-function migrateV0146() {
+async function migrateV0146() {
   const {version} = ENV;
   const key = 'mx-wc-config-migrated-0.1.46'
-  MxWcStorage.get(key, false).then((isMigrated) => {
-    if(isMigrated) {
-      console.info(key);
-    } else {
-      migrateConfigToV0146();
-      MxWcStorage.set(key, true);
-    }
-  });
+  const isMigrated = await MxWcStorage.get(key, false);
+  if(isMigrated) {
+    console.info(key);
+  } else {
+    await migrateConfigToV0146();
+    await MxWcStorage.set(key, true);
+  }
 }
 
-function migrateV0147() {
+async function migrateV0147() {
   const {version} = ENV;
   const key = 'mx-wc-config-migrated-0.1.47'
-  MxWcStorage.get(key, false).then((isMigrated) => {
-    if(isMigrated) {
-      console.info(key);
-    } else {
-      migrateConfigToV0147();
-      MxWcStorage.set(key, true);
-    }
-  });
+  const isMigrated = await MxWcStorage.get(key, false);
+  if(isMigrated) {
+    console.info(key);
+  } else {
+    await migrateConfigToV0147();
+    await MxWcStorage.set(key, true);
+  }
 }
 
-function migrateStorageToV0134() {
-  MxWcStorage.get('downloadFold')
-  .then((v) => {
-    MxWcStorage.set('downloadFolder', v);
-  });
+async function migrateStorageToV0134() {
+  const v = await MxWcStorage.get('downloadFold');
+  if (v) {
+    await MxWcStorage.set('downloadFolder', v);
+  }
 }
 
 // in version 0.1.34
@@ -79,29 +80,28 @@ function migrateStorageToV0134() {
 //     $MX-WC -> $STORAGE-PATH
 //
 //
-function migrateConfigToV0134() {
-  MxWcConfig.load().then((config) => {
-    if(config.clippingHandlerName) {
-      // config version < 0.1.34
-      config.clippingHandler =  T.capitalize(config.clippingHandlerName);
-      if(config.clippingHandlerName == 'native-app') {
-        config.handlerNativeAppEnabled = true;
-      }
-
-      config.hotkeySwitchEnabled = config.enableSwitchHotkey;
-      config.mouseModeEnabled = config.enableMouseMode;
-      config.titleStyleClippingFolderFormat = config.titleClippingFolderFormat;
-      config.titleStyleClippingFolderEnabled = config.saveTitleAsFoldName;
-
-      config.assetPath = config.assetPath.replace('$CLIP-FOLD', '$CLIPPING-PATH').replace('$MX-WC', '$STORAGE-PATH');
-      config.clippingJsPath = config.clippingJsPath.replace('$MX-WC', '$STORAGE-PATH');
-
-      MxWcStorage.set('config', config);
-      console.debug("0.1.34 migrate");
-    } else {
-      // config version >= 0.1.34
+async function migrateConfigToV0134() {
+  const config = await MxWcConfig.load()
+  if(config.clippingHandlerName) {
+    // config version < 0.1.34
+    config.clippingHandler =  T.capitalize(config.clippingHandlerName);
+    if(config.clippingHandlerName == 'native-app') {
+      config.handlerNativeAppEnabled = true;
     }
-  });
+
+    config.hotkeySwitchEnabled = config.enableSwitchHotkey;
+    config.mouseModeEnabled = config.enableMouseMode;
+    config.titleStyleClippingFolderFormat = config.titleClippingFolderFormat;
+    config.titleStyleClippingFolderEnabled = config.saveTitleAsFoldName;
+
+    config.assetPath = config.assetPath.replace('$CLIP-FOLD', '$CLIPPING-PATH').replace('$MX-WC', '$STORAGE-PATH');
+    config.clippingJsPath = config.clippingJsPath.replace('$MX-WC', '$STORAGE-PATH');
+
+    await MxWcStorage.set('config', config);
+    console.debug("0.1.34 migrate");
+  } else {
+    // config version >= 0.1.34
+  }
 }
 
 // in version 0.1.46
@@ -123,52 +123,57 @@ function migrateConfigToV0134() {
 //   saveClippingInformation
 //     -> htmlSaveClippingInformation
 //     -> mdSaveClippingInformation
-function migrateConfigToV0146() {
-  MxWcConfig.load().then((config) => {
-    if (config.titleStyleClippingFolderFormat) {
-      // version < 0.1.46
-      if (config.saveTitleAsFilename) {
-        config.mainFileName = '$TITLE.$FORMAT';
-        config.saveTitleFile = false;
-      }
+async function migrateConfigToV0146() {
+  const config = await MxWcConfig.load()
+  if (config.titleStyleClippingFolderFormat) {
+    // version < 0.1.46
 
-      if (config.titleStyleClippingFolderEnabled) {
-        config.saveTitleFile = false;
-      }
+    // new attributes (set default value)
+    config.mainFileName = 'index.$FORMAT';
+    config.saveTitleFile = true;
+    config.clippingFolderName = '$YYYY-$MM-$DD-$TIME-INTSEC';
 
-      // clippingFolderName
-      let arr = [];
-      switch(config.defaultClippingFolderFormat) {
-        case '$FORMAT-B':
-          arr.push('$YYYY$MM$DD$HH$mm$SS');
-          break;
-        case '$FORMAT-C':
-          arr.push('$TIME-INTSEC')
-          break;
-        default:
-          arr.push('$YYYY-$MM-$DD-$TIME-INTSEC');
-      }
-
-      if (config.titleStyleClippingFolderEnabled) {
-        if (config.titleClippingFolderFormat === '$FORMAT-B') {
-          config.clippingFolderName = '$TITLE';
-        } else {
-          arr.push('$TITLE');
-          config.clippingFolderName = arr.join('-');
-        }
-      }
-
-      config.assetFolder = config.assetPath;
-      config.htmlSaveClippingInformation = config.saveClippingInformation;
-      config.mdSaveClippingInformation = config.saveClippingInformation;
-
-      MxWcStorage.set('config', config);
-      console.debug("0.1.46 migrate");
-
-    } else {
-      // version >= 0.1.46
+    if (config.saveTitleAsFilename) {
+      config.mainFileName = '$TITLE.$FORMAT';
+      config.saveTitleFile = false;
     }
-  });
+
+    if (config.titleStyleClippingFolderEnabled) {
+      config.saveTitleFile = false;
+    }
+
+    // clippingFolderName
+    let arr = [];
+    switch(config.defaultClippingFolderFormat) {
+      case '$FORMAT-B':
+        arr.push('$YYYY$MM$DD$HH$mm$SS');
+        break;
+      case '$FORMAT-C':
+        arr.push('$TIME-INTSEC')
+        break;
+      default:
+        arr.push('$YYYY-$MM-$DD-$TIME-INTSEC');
+    }
+
+    if (config.titleStyleClippingFolderEnabled) {
+      if (config.titleClippingFolderFormat === '$FORMAT-B') {
+        config.clippingFolderName = '$TITLE';
+      } else {
+        arr.push('$TITLE');
+        config.clippingFolderName = arr.join('-');
+      }
+    }
+
+    config.assetFolder = config.assetPath;
+    config.htmlSaveClippingInformation = config.saveClippingInformation;
+    config.mdSaveClippingInformation = config.saveClippingInformation;
+
+    await MxWcStorage.set('config', config);
+    console.debug("0.1.46 migrate");
+
+  } else {
+    // version >= 0.1.46
+  }
 }
 
 //
@@ -176,14 +181,13 @@ function migrateConfigToV0146() {
 //
 // fix mainFileName $TITLE => $TITLE.$FORMAT
 //
-function migrateConfigToV0147() {
-  MxWcConfig.load().then((config) => {
-    if (config.mainFileName === '$TITLE') {
-      config.mainFileName = '$TITLE.$FORMAT';
-    }
-    MxWcStorage.set('config', config);
-    console.debug("0.1.47 migrate");
-  });
+async function migrateConfigToV0147() {
+  const config = await MxWcConfig.load();
+  if (config.mainFileName === '$TITLE') {
+    config.mainFileName = '$TITLE.$FORMAT';
+    await MxWcStorage.set('config', config);
+  }
+  console.debug("0.1.47 migrate");
 }
 
 const Migration = { perform: perform }
