b3ebac0ac867ff96ed480ffc5645efb4e217e9a0
mika <mika@nothing.org>
2020-11-15 18:43:07 +0800

    FIX enhance debug page

--- src/pages/debug.js
+++ src/pages/debug.js
@@ -1,9 +1,10 @@
 
 
-import T      from '../js/lib/tool.js';
-import ExtMsg from '../js/lib/ext-msg.js';
+import T           from '../js/lib/tool.js';
+import ExtMsg      from '../js/lib/ext-msg.js';
 import MxWcStorage from '../js/lib/storage.js';
-import MxWcConfig from '../js/lib/config.js';
+import MxWcConfig  from '../js/lib/config.js';
+import MxWcLink    from '../js/lib/link.js';
 
 
 import './_base.css';
@@ -92,6 +93,7 @@ async function renderStorage() {
 
 
 function init() {
+  MxWcLink.listen();
   initListener();
   renderAssetCache();
   renderStorage();
