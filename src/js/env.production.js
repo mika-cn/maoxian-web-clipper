c312cba17874c12d06ad511e1b17432330c24451
mika <mika@nothing.org>
2020-11-28 18:38:05 +0800

    RELEASE bump version to 0.1.68

--- src/js/env.production.js
+++ src/js/env.production.js
@@ -7,7 +7,7 @@ const mxAssistantRoot = [websiteRoot, 'assistant'].join('/');
 const env = {
   isDev: false,
   logLevel: "warn",
-  version: '0.1.67',
+  version: '0.1.68',
   minNativeAppVersion: '0.2.2',
   websiteRoot: websiteRoot,
   projectRoot: projectRoot,
