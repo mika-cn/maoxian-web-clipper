c991f18402984a0e18b1968fa9f5b971e34c524d
mika <mika@nothing.org>
2021-05-13 18:05:27 +0800

    RELEASE bump version to 0.1.76

--- src/js/env.js
+++ src/js/env.js
@@ -7,7 +7,7 @@ const mxAssistantRoot = [websiteRoot, 'tmp/assistant'].join('/');
 const env = {
   isDev: true,
   logLevel: "debug",
-  version: '0.1.75',
+  version: '0.1.76',
   minNativeAppVersion: '0.2.2',
   websiteRoot: websiteRoot,
   projectRoot: projectRoot,
