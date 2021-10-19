793bd6e78b9679badc2304507b851c56042ed89c
mika <mika@nothing.org>
2021-10-19 14:28:57 +0800

    RELEASE bump version to 0.2.5

--- src/js/env.production.js
+++ src/js/env.production.js
@@ -7,7 +7,7 @@ const mxAssistantRoot = [websiteRoot, 'assistant'].join('/');
 const env = {
   isDev: false,
   logLevel: "warn",
-  version: '0.2.4',
+  version: '0.2.5',
   minNativeAppVersion: '0.2.8',
   websiteRoot: websiteRoot,
   projectRoot: projectRoot,
