23602b917d146d0c740bc63f01d6e605b35c5979
mika <mika@nothing.org>
2022-01-02 12:55:10 +0800

    NEW apply filterList

--- src/js/capturer/stylesheet.js
+++ src/js/capturer/stylesheet.js
@@ -68,82 +68,116 @@ async function captureStyleObj(styleObj, params) {
 
 function getResourceHandler(params) {
   const {tasks, ownerType, baseUrl, docBaseUrl, storageInfo, clipId, config, requestParams} = params;
-  // @return path;
-  return async ({ownerType, resourceType, cssText, baseUrl, url}) => {
-    const saveResource = (
-         resourceType == 'css'   && true
-      || resourceType == 'image' && config.htmlCaptureCssImage == 'saveAll'
-      || resourceType == 'font'  && saveWebFont(config, url)
+  // @return paths;
+  return async ({ownerType, resourceType, baseUrl, resourceItems}) => {
+    const boolFlags = (
+         resourceType == 'css'   && saveAll(resourceItems)
+      || resourceType == 'image' && getImageFlags(resourceItems, config)
+      || resourceType == 'font'  && getWebFontFlags(resourceItems, config)
     );
 
-    if (!saveResource) { return '' }
+    const paths = [];
+    for (let i = 0; i < resourceItems.length; i++) {
+      const {url, cssText} = resourceItems[i];
+      const saveResource = boolFlags[i];
+      if (!saveResource) {
+        paths.push('');
+        continue;
+      }
+
+      const extension = (resourceType == 'css' ? 'css' : undefined);
+
+      const mimeTypeData = {};
+      if (!cssText) {
+        // images or fonts
+        mimeTypeData.httpMimeType = await Asset.getHttpMimeType(
+          requestParams.toParams(url));
+      }
+
+      const name = Asset.getNameByLink({
+        template: storageInfo.raw.assetFileName,
+        link: url,
+        extension: extension,
+        mimeTypeData: mimeTypeData,
+        now: storageInfo.valueObj.now,
+      });
+
+      const assetName = await Asset.getUniqueName({
+        clipId: clipId,
+        id: url,
+        folder: storageInfo.assetFolder,
+        filename: name
+      });
+
+      const filename = T.joinPath(storageInfo.assetFolder, assetName);
+
+      let task;
+      switch(resourceType) {
+        case 'image': {
+          task = Task.createImageTask(filename, url, clipId, requestParams);
+          break;
+        }
+        case 'font' : {
+          task = Task.createFontTask(filename, url, clipId, requestParams);
+          break;
+        }
+        case 'css'  : {
+          task = Task.createStyleTask(filename, cssText, clipId);
+          break;
+        }
+        default:
+          throw new Error("Unknown resourceType: " + resourceType)
+      }
+      tasks.push(task);
 
 
-    const extension = (resourceType == 'css' ? 'css' : undefined);
-
-    const httpMimeType = await Asset.getHttpMimeType(requestParams.toParams(url));
-
-    const name = Asset.getNameByLink({
-      template: storageInfo.raw.assetFileName,
-      link: url,
-      extension: extension,
-      mimeTypeData: {httpMimeType},
-      now: storageInfo.valueObj.now,
-    });
-
-    const assetName = await Asset.getUniqueName({
-      clipId: clipId,
-      id: url,
-      folder: storageInfo.assetFolder,
-      filename: name
-    });
-
-    const filename = T.joinPath(storageInfo.assetFolder, assetName);
-
-    let task;
-
-    switch(resourceType) {
-      case 'image': tasks.push(Task.createImageTask(filename, url, clipId, requestParams)); break;
-      case 'font' : tasks.push(Task.createFontTask(filename, url, clipId, requestParams)); break;
-      case 'css'  : tasks.push(Task.createStyleTask(filename, cssText, clipId)); break;
-      default:
-        throw new Error("Unknown resourceType: " + resourceType)
+      if(baseUrl === docBaseUrl) {
+        // In this case, it means this asset is referred by document (top document or iframe).
+        // We need to calculate path that relative to mainFileFolder,
+        // which is the place that the document will be saved in.
+        paths.push(T.calcPath(storageInfo.mainFileFolder, filename));
+      } else {
+        // This asset is referred by an external style which is also an asset.
+        // So they will stay in same folder.
+        paths.push(assetName);
+      }
     }
-
-
-    if(baseUrl === docBaseUrl) {
-      // In this case, it means this asset is referred by document (top document or iframe).
-      // We need to calculate path that relative to mainFileFolder,
-      // which is the place that the document will be saved in.
-      return T.calcPath(storageInfo.mainFileFolder, filename);
-    } else {
-      // This asset is referred by an external style which is also an asset.
-      // So they will stay in same folder.
-      return assetName;
-    }
+    return paths;
   }
 }
 
+// @returns {[boolean]} flags
+function saveAll(resourceItems) { return resourceItems.map(() => true) }
+function removeAll(resourceItems) { return resourceItems.map(() => false) }
 
-// save web font or not?
-// returns a boolean value.
-function saveWebFont(config, url) {
+// @return {[Boolean]} flags
+function getImageFlags(resourceItems, config) {
+  if (config.htmlCaptureCssImage == 'saveAll') {
+    return saveAll(resourceItems);
+  } else {
+    return removeAll(resourceItems);
+  }
+}
+
+// @return {[Boolean]} flags
+function getWebFontFlags(resourceItems, config) {
   const option = config.htmlCaptureWebFont;
-  let filterText;
-  switch(config.htmlCaptureWebFont) {
-    case 'remove': return false;
-    case 'saveAll': return true;
-    case 'saveWoff': {
-      filterText = 'woff,woff2';
-      break;
+  switch(option) {
+    case 'remove': return removeAll(resourceItems);
+    case 'saveAll': return saveAll(resourceItems);
+    case 'filterList': {
+      return CaptureTool.matchFilterList(
+        config.htmlWebFontFilterList,
+        resourceItems,
+        saveAll
+      );
     }
-    case 'filter': {
-      filterText = config.htmlWebFontFilter;
-      break;
+    default: {
+      throw new Error(`Unknow option, htmlCaptureWebFont : ${option}`);
     }
   }
-  return CaptureTool.isFilterMatch(filterText, url);
 }
+
 
 
 function fixBodyChildrenStyle(css) {
