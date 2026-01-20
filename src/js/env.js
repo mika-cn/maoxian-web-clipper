502e45656e6f7aa3ddf57c0c9b7eca0d3816619b
mika <mika@nothing.org>
2026-01-20 16:37:33 +0800

    RELEASE bump version to 0.7.0.9

--- src/js/env.js
+++ src/js/env.js
@@ -7,7 +7,7 @@ const mxAssistantRoot = [websiteRoot, 'tmp/assistant'].join('/');
 const env = {
   isDev: true,
   logLevel: "debug",
-  version: '0.7.0.8',
+  version: '0.7.0.9',
   minNativeAppVersion: '0.2.8',
   websiteRoot: websiteRoot,
   projectRoot: projectRoot,
